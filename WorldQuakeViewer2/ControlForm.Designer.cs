b25789398dbb9bb2fe8167c7a59ae900d7a9db43
Ichihai1415 <projects3141.gif@gmail.com>
2025-05-15 17:10:10 +0900

    旧構成をコピー

--- WorldQuakeViewer2/ControlForm.Designer.cs
+++ WorldQuakeViewer2/ControlForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WorldQuakeViewer2
 {
-    partial class Form1
+    partial class ControlForm
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            SuspendLayout();
+            // 
+            // CtrlForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Name = "CtrlForm";
+            Text = "Form1";
+            Load += CtrlForm_Load;
+            ResumeLayout(false);
         }
 
         #endregion
---
+++ WorldQuakeViewer2/ControlForm.cs
@@ -0,0 +1,94 @@
+using System.Data;
+using System.Drawing.Imaging;
+using System.Media;
+using System.Text;
+using LL2FERC;
+
+namespace WorldQuakeViewer2
+{
+    public partial class ControlForm : Form
+    {
+        public ControlForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// バージョン(プロジェクトのバージョン、_versionも変えること)
+        /// </summary>
+        public const string Version = "1.0.0-α1";
+
+        /// <summary>
+        /// ダイアログ等を最前面に表示する用
+        /// </summary>
+        internal static Form topMost = new() { TopMost = true };
+
+        /// <summary>
+        /// 文字描画用フォント
+        /// </summary>
+        internal static FontFamily font = new("koruri");
+
+        /// <summary>
+        /// 震央マーク用色置換
+        /// </summary>
+        internal static ImageAttributes ia = new ImageAttributes();
+
+        /// <summary>
+        /// staticでアクセスできるようにしたログ表示テキストボックス
+        /// </summary>
+        internal static TextBox logTextBox;
+
+        /// <summary>
+        /// 設定
+        /// </summary>
+        internal static Config config = new();
+
+        /// <summary>
+        /// データのリスト
+        /// </summary>
+        internal static Data data = new();
+
+        /// <summary>
+        /// 取得用
+        /// </summary>
+        internal static HttpClient client = new HttpClient();
+
+        /// <summary>
+        /// 音声再生用
+        /// </summary>
+        internal static SoundPlayer? player = null;
+
+        /// <summary>
+        /// 実行ログの高速追加用
+        /// </summary>
+        internal static StringBuilder exeLogs = new StringBuilder();
+
+        /// <summary>
+        /// 更新処理の無効
+        /// </summary>
+        internal static bool noFirst = false;
+
+        /// <summary>
+        /// 表示画面の配列
+        /// </summary>
+        internal static List<DataView> dataViews = [];
+
+        /// <summary>
+        /// 過去情報表示画面
+        /// </summary>
+        internal static DataView pastDataView = new();
+
+        /// <summary>
+        /// ユーザー震央辞書
+        /// </summary>
+        internal static LL2FERC.LL2FERC.FromFile? hypoUser = null;
+
+        /// <summary>
+        /// 読み込み時初期化処理
+        /// </summary>
+        private async void CtrlForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
